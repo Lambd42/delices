b1228a218917be7d227701f64a75fb6ce996dcec
Lambda <tom.billet@ecoles-epsi.net>
2024-10-01 11:54:33 +0200

    page /liste-typebonbon

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Form\AddCandyType;
+use App\Repository\TypeBonbonRepository;
 
 class BaseController extends AbstractController
 {
@@ -23,6 +24,15 @@ class BaseController extends AbstractController
         $form = $this->createForm(AddCandyType::class);
         return $this->render('base/add_candy.html.twig', [
             'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/liste-typebonbon', name: 'app_typebonbon_list')]
+    public function typebonbon_list(TypeBonbonRepository $typeBonbonRepository): Response
+    {
+        $all_typebonbon = $typeBonbonRepository->findAll();
+        return $this->render('base/typebonbon_list.html.twig', [
+            'all_typebonbon' => $all_typebonbon
         ]);
     }
 }
